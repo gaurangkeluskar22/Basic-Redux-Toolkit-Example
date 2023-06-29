e687636a165e883961e60b38359b30e26d2e1211
gaurangkeluskar22 <gaurangkeluskar22@gmail.com>
2023-06-29 20:37:55 +0530

    Added code for store

--- src/index.js
+++ src/index.js
@@ -6,11 +6,15 @@ import reportWebVitals from './reportWebVitals';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import store from './Redux/store';
+import {Provider} from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
