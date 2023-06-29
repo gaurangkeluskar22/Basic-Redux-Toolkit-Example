95138798b5ae42a995c28de3a727bd33a73b412f
gaurangkeluskar22 <gaurangkeluskar22@gmail.com>
2023-06-29 23:48:34 +0530

    Added incermentByAmount functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { useState } from 'react';
 import {useSelector, useDispatch} from 'react-redux';
-import { decrement, increment } from './Redux/counter';
+import { decrement, increment, incrementByAmount } from './Redux/counter';
 
 function App() {
   const count = useSelector((state)=>state.counter.value);
   const dispatch = useDispatch();
+  const [counterInputValue, setCounterInputValue] = useState();
+
+  const handleChange=(e)=>{
+    e.preventDefault();
+    const value = e.target.value;
+    setCounterInputValue(value);
+  }
+  
   return (
     <>
       <div className="container text-center mt-5">
@@ -24,6 +33,8 @@ function App() {
           <button className="p-2 m-2" onClick={()=>dispatch(increment())}>+</button>
             <span className="p-2 m-2">{count}</span>
             <button className="p-2 m-2" onClick={()=>dispatch(decrement())}>-</button>
+            <input type="number" name="input" onChange={(e)=> handleChange(e)}></input>
+            <button onClick={()=>dispatch(incrementByAmount(counterInputValue))}>Submit</button>
           </div>
           <div className="col-sm-4">
           </div>
